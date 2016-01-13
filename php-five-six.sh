6cd63af6dcc39afedf8053bb4d4d48dd96522f67
Rudi Strydom <iam@thatguy.co.za>
2016-01-13 23:28:16 +0200

    Ensuring that the docker is dead before firing up a new onw

--- php-five-six.sh
+++ php-five-six.sh
@@ -1,7 +1,8 @@
 # Use this to manually run a script
 # Usage: ./php-cli.sh script.php
 echo "Running php-cli Docker with arguments $@"
-docker run -it --rm --name php-cli \
+docker rm -f php56-cli 2>/dev/null
+docker run -it --rm --name php56-cli \
 -v "$PWD":/usr/src/myapp \
 -v "${HOME}/Music":/usr/src/myapp/music \
 -w /usr/src/myapp \
--- php-seven.sh
+++ php-seven.sh
@@ -1,7 +1,8 @@
 # Use this to manually run a script
 # Usage: ./php-cli.sh script.php
 echo "Running php-cli Docker with arguments $@"
-docker run -it --rm --name php-cli \
+docker rm -f php7-cli 2>/dev/null
+docker run -it --rm --name php7-cli \
 -v "$PWD":/usr/src/myapp \
 -v "${HOME}/Music":/usr/src/myapp/music \
 -w /usr/src/myapp \
