b2722ab95c61a66466b96fd57bc4a4b062b70712
Rudi Strydom <iam@thatguy.co.za>
2016-01-21 23:04:08 +0200

    Clearing screen on scripts

--- php-five-six-simple.sh
+++ php-five-six-simple.sh
@@ -5,8 +5,5 @@ echo "Running php-cli Docker with arguments $@"
 docker rm -f php56-cli 2>/dev/null
 docker run -it --rm --name php56-cli \
 -v "$PWD":/usr/src/myapp \
--v "${HOME}/Music":/usr/src/myapp/music \
--v "${HOME}/Pictures/wallpapers":/usr/src/myapp/pictures \
--v "${HOME}":/usr/src/myapp/home \
 -w /usr/src/myapp \
 php:5.6-cli php "$@"
--- php-seven-simple.sh
+++ php-seven-simple.sh
@@ -1,5 +1,6 @@
 # Use this to manually run a script
 # Usage: ./php-cli.sh script.php
+clear
 echo "Running php-cli Docker with arguments $@"
 docker rm -f php7-cli 2>/dev/null
 docker run -it --rm --name php7-cli \
