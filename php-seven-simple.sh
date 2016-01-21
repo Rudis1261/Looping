35e44050e652a7f8986fc2dccb0b077c76da29dd
Rudi Strydom <iam@thatguy.co.za>
2016-01-21 22:51:37 +0200

    Creating fallback docker run script

--- php-seven-simple.sh
+++ php-seven-simple.sh
@@ -4,7 +4,5 @@ echo "Running php-cli Docker with arguments $@"
 docker rm -f php7-cli 2>/dev/null
 docker run -it --rm --name php7-cli \
 -v "$PWD":/usr/src/myapp \
--v "${HOME}/Music":/usr/src/myapp/music \
--v "${HOME}/Pictures/Wallpapers":/usr/src/myapp/pictures \
 -w /usr/src/myapp \
 drpain/php-cli:latest php "$@"
