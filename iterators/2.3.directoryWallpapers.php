044a86ef8f8034a2ebf8d72bdbc46140ab53d43b
Rudi Strydom <iam@thatguy.co.za>
2016-01-21 22:58:36 +0200

    Bordering the rediculous

--- iterators/2.3.directoryWallpapers.php
+++ iterators/2.3.directoryWallpapers.php
@@ -1,10 +1,12 @@
 <?php
 require 'functions.inc.php';
 // Instantiate the iterator. Skipping Dots
-$iter = new RecursiveDirectoryIterator("music", FilesystemIterator::SKIP_DOTS);
+$iter = new RecursiveDirectoryIterator("pictures", FilesystemIterator::SKIP_DOTS);
 
 // Iterate over them as usual
+$count = 1;
 foreach (new RecursiveIteratorIterator($iter) as $item) {
-    echo $item . PHP_EOL;
+    echo $count . ": " .  $item . PHP_EOL;
+    $count++;
 }
 printStatus($iter);
---
+++ iterators/2.4.homeDirectory.php
@@ -0,0 +1,12 @@
+<?php
+require 'functions.inc.php';
+// Instantiate the iterator. Skipping Dots
+$iter = new RecursiveDirectoryIterator("home", FilesystemIterator::SKIP_DOTS);
+
+// Iterate over them as usual
+$count = 1;
+foreach (new RecursiveIteratorIterator($iter) as $item) {
+    echo $count . ": " .  $item . PHP_EOL;
+    $count++;
+}
+printStatus($iter);
